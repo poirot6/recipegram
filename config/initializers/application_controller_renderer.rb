12769498eaa0e89bf742e7bdbb4c1c2435ae505e
Ubuntu <ubuntu@ip-172-31-5-188.ap-northeast-1.compute.internal>
2021-04-16 07:59:44 +0000

    users

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'f6d90a9426574507a92317cbd78ca4fd52b7f1f5006ac09c4cf46e2da2e963cca25da8344755e765d86d6c765e47f58ed6d333822cf74eb31df50accdf0c8026'
