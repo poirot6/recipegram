76e12e0538f8a899aa81757b3c8e018c31f84bef
Ubuntu <ubuntu@ip-172-31-5-188.ap-northeast-1.compute.internal>
2021-04-17 04:14:14 +0000

    validate

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,21 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+  
   def index
     @users = User.all
   end
+  
   def show
     @user = User.find(params[:id])
   end
+  
   def edit
     @user = User.find(params[:id])
     if @user != current_user
         redirect_to user_path(current_user), alert: "不正なアクセスです。"
     end
   end
+  
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
